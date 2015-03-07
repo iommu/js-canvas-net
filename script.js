f8d2cdc810639aabe087060590e47e4a04f23bea
Alex Carter <acarter010204@gmail.com>
2015-03-08 09:46:02 +1100

    fixed black line issue

---
+++ script.js
@@ -0,0 +1,58 @@
+//Animate
+// Request anim
+(function () {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+//Generate canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.height = document.body.clientHeight;
+canvas.width = document.body.clientWidth;
+document.body.appendChild(canvas);
+//Variables
+var circles = [];
+var num = 20;
+//Functions
+var initialize = function() {
+    circles = [];
+    for ( var x=0; x<num; x++ ) {
+        var sx = (Math.random() * (canvas.width+100)) - 50;
+        var sy = (Math.random() * (canvas.height+100)) - 50;
+        var circle = {
+            sx: sx,
+            sy: sy,
+            x: sx,
+            y: sy,
+            r: (Math.random() * 3 + 6 ),
+            opacity: .2,
+            direction: Math.random() > .5? 1:-1
+        };
+        circles.push( circle );
+    }
+};
+
+var animate  = function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var old = {
+      x: circles[(num - 1)].x,
+      y: circles[(num - 1)].y
+    }
+    for ( var x=0; x<circles.length; x++ ) {
+        circle = circles[x];
+        ctx.beginPath();
+        ctx.moveTo(old.x,old.y);
+        ctx.lineTo(circle.x,circle.y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.lineWidth = .8;
+        ctx.strokeStyle = "rgba(50,146,118," + circle.opacity + ")";
+        ctx.arc( circle.x, circle.y, circle.r, 0, Math.PI *2, true );
+        ctx.stroke();
+        ctx.closePath();
+    }
+    // requestAnimationFrame( animate );
+};
+//Init
+initialize();
+animate();
